72b915dadc5b94689518016042434aaea5313569
Michael Wilson <michael.wilson21@ontariotechu.net>
2021-11-11 12:44:56 -0500

    Added A bunch of Assets and Scripts

---
+++ REALMS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject playerObject;
+
+    public Transform pivot;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, pivot.position + offset, 0.1f);
+    }
+
+    public void CameraFix()
+    {
+        transform.parent = playerObject.transform;
+        enabled = true;
+        GetComponent<MouseLook>().enabled = true;
+    }
+
+}
---
+++ REALMS/Assets/Scripts/LanternController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour
+{
+    public Transform target;
+
+    bool moving;
+
+    public List<Transform> waypoints = new List<Transform>();
+    int currWaypoint = -1;
+
+    public float lerpTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    IEnumerator Move()
+    {
+        moving = true;
+        currWaypoint++;
+        target = waypoints[currWaypoint];
+
+        while ((transform.position - target.position).magnitude > 0.5f)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, target.position, lerpTime);
+            transform.position = newPosition;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        moving = false;
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            if (currWaypoint < waypoints.Count && !moving)
+            {
+                StopAllCoroutines();
+                StartCoroutine(Move());
+            }
+        }
+    }
+
+}
